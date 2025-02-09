b0c9bcd31aa8a8ef09635911dbc1cc70fa75724f
jensettl <ettljens@gmail.com>
2025-02-09 16:42:40 +0100

    add get_astronomy_data function to fetch astronomy data from WeatherAPI

--- services/weather_service.py
+++ services/weather_service.py
@@ -22,3 +22,25 @@ def get_weather_data(query: str):
     else:
         print(f"Failed to fetch weather data for {query}.")
         return None
+      
+
+def get_astronomy_data(query: str, date: str):
+    """
+    Fetch astronomy data from the WeatherAPI.
+    
+    :return: dict or None, astronomy data if successful, otherwise None
+    """
+    
+    url = "http://api.weatherapi.com/v1/astronomy.json"
+    params = {
+        "key": f"{config.WEATHER_API_KEY}",
+        "q": f"{query}",
+        "dt": f"{date}",
+    }
+    astronomy_data = fetch_data(url, params=params)
+    
+    if astronomy_data:
+        return astronomy_data
+    else:
+        print(f"Failed to fetch astronomy data for {query} on {date}.")
+        return None
