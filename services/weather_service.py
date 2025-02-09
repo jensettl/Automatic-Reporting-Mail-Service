0d9c2f6254369ce52787c92017976e6686a40231
jensettl <ettljens@gmail.com>
2025-02-09 16:39:27 +0100

    add get_weather_data function to fetch weather data from WeatherAPI

--- services/weather_service.py
+++ services/weather_service.py
@@ -1,1 +1,24 @@
 # Logic for fetching and handling weather data
+
+from utils.api_requests import fetch_data
+import config
+
+def get_weather_data(query: str):
+    """
+    Fetch weather data from the WeatherAPI.
+    
+    :return: dict or None, weather data if successful, otherwise None
+    """
+    
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": f"{config.WEATHER_API_KEY}",
+        "q": f"{query}",
+    }
+    weather_data = fetch_data(url, params=params)
+    
+    if weather_data:
+        return weather_data
+    else:
+        print(f"Failed to fetch weather data for {query}.")
+        return None
