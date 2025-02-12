59ed8012e456f28a06f61527e378fb13621b5ed6
jensettl <ettljens@gmail.com>
2025-02-12 13:32:30 +0100

    add calculateBarChartLength function to compute stock recommendation percentages

--- services/stocks_service.py
+++ services/stocks_service.py
@@ -2,6 +2,26 @@
 
 import yfinance as yf
 
+def calculateBarChartLength(
+    strongBuy: int, buy: int, hold: int, sell: int, strongSell: int
+):
+    sum = strongBuy + buy + hold + sell + strongSell
+
+    strongBuy = int((strongBuy / sum) * 100)
+    buy = int((buy / sum) * 100)
+    hold = int((hold / sum) * 100)
+    sell = int((sell / sum) * 100)
+    strongSell = int((strongSell / sum) * 100)
+
+    # Check if any value is 0 and change it to 1
+    strongBuy = strongBuy if strongBuy != 0 else 1
+    buy = buy if buy != 0 else 1
+    hold = hold if hold != 0 else 1
+    sell = sell if sell != 0 else 1
+    strongSell = strongSell if strongSell != 0 else 1
+
+    return strongBuy, buy, hold, sell, strongSell
+
 def get_stock_info(ticker_list):
     data = {}
     CURRENT_EXCHANGE_RATE = yf.Ticker("USDEUR=X").history(period="1d")["Close"].iloc[-1].round(2)
@@ -19,9 +39,15 @@ def get_stock_info(ticker_list):
         
         sector = stock.info.get("sector", "No data available")
         
-        recommondation = stock.recommendations.iloc[0][
+        # Get the recommendation of the stock
+        recommondation = stock.recommendations.iloc[0]
+        strongBuy, buy, hold, sell, strongSell = recommondation[
             ["strongBuy", "buy", "hold", "sell", "strongSell"]
-        ].to_string()
+        ]
+        # Calculate the length of the bar chart
+        strongBuy, buy, hold, sell, strongSell = calculateBarChartLength(
+            strongBuy, buy, hold, sell, strongSell
+        )
         
         data[ticker] = {
             "symbol": ticker,
@@ -29,7 +55,11 @@ def get_stock_info(ticker_list):
             "changePct": changePct,
             "changeSymbol": changeSymbol,
             "sector": sector,
-            "recommondation": recommondation,
+            "strongBuy": strongBuy,
+            "buy": buy,
+            "hold": hold,
+            "sell": sell,
+            "strongSell": strongSell,
         } 
         
     return data
