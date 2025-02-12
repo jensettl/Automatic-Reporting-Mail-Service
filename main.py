9793e8069372d9e20526b558a753cda566ccc9ed
jensettl <ettljens@gmail.com>
2025-02-12 13:50:14 +0100

    refactor main.py to use configuration for date and ticker list

--- main.py
+++ main.py
@@ -6,19 +6,15 @@ from services.stocks_service import get_stock_info
 from utils.data_cleaner import clean_weather_data, clean_astronomy_data, clean_stock_data
 from utils.email_builder import build_email_body, sendEmail
 
-from datetime import datetime
 import config
-import os
-css_file_path = os.path.join(os.path.dirname(__file__), "utils", "styles.css")
 
-
-TODAY = datetime.today().strftime('%Y-%m-%d')
-TICKER_LIST = ["GOOGL", "AAPL", "MSFT", "AMZN", "TSLA", "NVDA"]
+TODAY = config.TODAY
+TICKER_LIST = config.TICKER_LIST
 
 def main():
     # Fetch data 
-    weather_data = get_weather_data("Karlsruhe")
-    astronomy_data = get_astronomy_data("Karlsruhe", TODAY)
+    weather_data = get_weather_data(config.QUERY)
+    astronomy_data = get_astronomy_data(config.QUERY, TODAY)
     stock_data = get_stock_info(TICKER_LIST)
     
     # Clean and structure data
