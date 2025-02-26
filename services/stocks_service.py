5c13d441f583e71ae3cec6acac0dec1253ffdeb3
jensettl <ettljens@gmail.com>
2025-02-26 12:42:43 +0100

    add error handling and reduce number of requests of yfinance

--- services/stocks_service.py
+++ services/stocks_service.py
@@ -1,6 +1,7 @@
 # Logic for fetching and handling stock data
 
 import yfinance as yf
+from yfinance import exceptions as yf_exception
 
 def calculateBarChartLength(
     strongBuy: int, buy: int, hold: int, sell: int, strongSell: int
@@ -23,6 +24,8 @@ def calculateBarChartLength(
     return strongBuy, buy, hold, sell, strongSell
 
 def get_stock_info(ticker_list):
+    """ Fetches stock data for the given list of tickers """
+    
     data = {}
     
     try:
@@ -30,20 +33,26 @@ def get_stock_info(ticker_list):
     except(Exception):
         CURRENT_EXCHANGE_RATE = 1
     
+    try:
+        tickers = yf.Tickers(" ".join(ticker_list))  # Fetch data once for all tickers
+    except yf_exception.YFinanceError:
+        print("Could not fetch data for the given tickers")
+        return data
+    except Exception as e:
+        print(f"An error occured: {e}")
+        return data
+    
+    # Loop through the tickers and extract the data needed
     for ticker in ticker_list:
-        
         try:
-            stock = yf.Ticker(ticker)
-        except(yf.exceptions.YFinanceError):
+            stock = tickers.tickers[ticker]
+        except KeyError:
             print(f"Could not fetch data for {ticker}")
-            continue
-        except(Exception):
-            print(f"An error occured while fetching data for {ticker}")
             continue
         
         hist = stock.history(period="5d")
         
-        closing_price = (hist["Close"].iloc[-1]*CURRENT_EXCHANGE_RATE).round(2)
+        closing_price = (hist["Close"].iloc[-1] * CURRENT_EXCHANGE_RATE).round(2)
         
         changePct = hist["Close"].pct_change().round(2).iloc[-1] * 100
         changeSymbol = "🔺" if changePct > 0 else " " if changePct == 0 else "🔻"
@@ -51,14 +60,17 @@ def get_stock_info(ticker_list):
         sector = stock.info.get("sector", "No data available")
         
         # Get the recommendation of the stock
-        recommondation = stock.recommendations.iloc[0]
-        strongBuy, buy, hold, sell, strongSell = recommondation[
-            ["strongBuy", "buy", "hold", "sell", "strongSell"]
-        ]
-        # Calculate the length of the bar chart
-        strongBuy, buy, hold, sell, strongSell = calculateBarChartLength(
-            strongBuy, buy, hold, sell, strongSell
-        )
+        try:
+            recommendation = stock.recommendations.iloc[0]
+            strongBuy, buy, hold, sell, strongSell = recommendation[
+                ["strongBuy", "buy", "hold", "sell", "strongSell"]
+            ]
+            # Calculate the length of the bar chart
+            strongBuy, buy, hold, sell, strongSell = calculateBarChartLength(
+                strongBuy, buy, hold, sell, strongSell
+            )
+        except (IndexError, KeyError):
+            strongBuy = buy = hold = sell = strongSell = 0
         
         data[ticker] = {
             "symbol": ticker,
@@ -71,6 +83,9 @@ def get_stock_info(ticker_list):
             "hold": hold,
             "sell": sell,
             "strongSell": strongSell,
-        } 
+        }
         
     return data
+
+if __name__ == "__main__":
+    print(get_stock_info(["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA"]))
